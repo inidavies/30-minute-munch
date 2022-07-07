3e56c1c160f32a4ed5d4f690141538a3a31c98c5
Ini Davies <inidavies24@gmail.com>
2022-07-07 15:57:09 +0000

    style edit

--- unit-test.py
+++ unit-test.py
@@ -5,7 +5,7 @@ from 3-min-munch import get_user_filter, get_recipes, create_recipe_db, display_
 class Unit_test(unittest.TestCase):
     def test_get_user_ingredient(self):
         self.assertEqual(get_user_ingredient(), None)
-  
+
     # To be added once main code works
     '''def test_get_restrictions(self):
         self.assertEqual(get_restrictions(), None)'''
