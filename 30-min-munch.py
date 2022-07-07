386622a931f1a6d4a2086fb121477c339420fe75
Ini Davies <inidavies24@gmail.com>
2022-07-07 15:33:03 +0000

    style edit

--- 30-min-munch.py
+++ 30-min-munch.py
@@ -5,7 +5,8 @@ import sqlalchemy as db
 API_KEY = "ceeb271bddmshe389392044c13efp170620jsn7cfdf68bc735"
 API_HOST = "tasty.p.rapidapi.com"
 
-#Prompts the user to enter a valid ingredient (strings only) and returns that ingredient
+''' Prompts the user to enter a valid ingredient (strings only)
+ and returns that ingredient '''
 def get_user_ingredient():
     ingredient = input('What ingredient do you want to see 30-minute recipes for?: ')
     try:
@@ -15,7 +16,8 @@ def get_user_ingredient():
         pass
     return ingredient
 
-#This should return a dictionary of recipes
+
+# This should return a dictionary of recipes
 def get_recipes(app_key, app_host, ingredient):
     url = "https://tasty.p.rapidapi.com/recipes/list"
 
@@ -28,6 +30,7 @@ def get_recipes(app_key, app_host, ingredient):
     response = requests.get(url, headers=headers, params=querystring)
     return response.json()["results"]
 
+
 def create_recipe_db(ingredient):
     recipe_list = get_recipes(API_KEY, API_HOST, ingredient)
 
@@ -37,7 +40,6 @@ def create_recipe_db(ingredient):
 
     for recipe in recipe_list:
         # Put the data from the post in the pandas dataframe
-        #munchies.loc[len(munchies.index)] = [recipe["name"], "NULL", "NULL", "NULL", "NULL]"]
         if "prep_time_minutes" in recipe and "cook_time_minutes" in recipe and "total_time_minutes" in recipe and "video_url" in recipe and "description" in recipe and "instructions" in recipe:
             
             #Generate a string of instructions
@@ -56,8 +58,9 @@ def create_recipe_db(ingredient):
     # Create and send sql table from your dataframe
     munchies.to_sql('recipes', con=engine, if_exists='replace', index=False)
     
-    #Return Database Query
+    # Return Database Query
     return engine.execute("SELECT * FROM recipes;").fetchall()
+
 
 def display_recipe_db(query_result, ingredient):
     if query_result != []:
